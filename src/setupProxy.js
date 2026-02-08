b572051fa9fb3c5e0e6995a7ab05bda496547084
woody_home <seho.jang@inspien.co.kr>
2026-02-08 16:51:44 +0900

    For move to AWS

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  const target = process.env.REACT_APP_API_TARGET || 'http://127.0.0.1:8080';
+
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://127.0.0.1:443/api',
+      target,
       changeOrigin: true,
       secure: false,
       logLevel: 'debug'
